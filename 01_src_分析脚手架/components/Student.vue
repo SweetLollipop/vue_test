d46d6823b51432739d3786ad922f16d5454b7fa5
SweetLollipop <137623637@qq.com>
2022-01-22 21:16:42 +0800

    分析脚手架

---
+++ 01_src_分析脚手架/components/Student.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生名称:{{studentName}}</h2>
+        <h2>学生年龄:{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码（数据、方法等等）
+    export default{
+        name:'Student',
+        data(){
+            return{
+                studentName:'张三',
+                age:18,
+            }
+        },
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
