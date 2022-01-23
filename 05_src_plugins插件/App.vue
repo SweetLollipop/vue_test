718a8c9c1ab214249c286fb7c5d358e11f22793a
SweetLollipop <137623637@qq.com>
2022-01-23 17:06:40 +0800

    05_src_plugins插件

---
+++ 05_src_plugins插件/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <School/>
+        <hr>
+        <Student/>
+    </div>
+</template>
+
+<script>
+    // 引入组件
+    import School from './components/School.vue'
+    import Student from './components/Student.vue'
+    export default {
+        name:'App',
+        data(){
+            return{
+                
+            }
+        },
+        components:{
+            School,
+            Student,
+        },
+        methods:{
+           
+        }
+    }
+</script>
+
+<style>
+
+</style>
