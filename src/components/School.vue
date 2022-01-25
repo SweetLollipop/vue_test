71ecb3528b0a2c5190297190ee8744617de8f64a
SweetLollipop <137623637@qq.com>
2022-01-26 00:25:09 +0800

    080组件自定义事件_绑定

---
+++ src/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="school">
+        <h2>学校名称:{{schoolName}}</h2>
+        <h2>学校地址:{{address}}</h2>
+        <button @click="sendSchoolName">把学校名传给App</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码（数据、方法等等）
+    export default{
+        name:'School',
+        props:['getSchoolName'],
+        data(){
+            return{
+                schoolName:'尚硅谷',
+                address:'北京',
+            }
+        },
+        methods:{
+            sendSchoolName(){
+                this.getSchoolName(this.schoolName)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /* 组件的样式 */
+    .school{
+        background-color: skyblue;
+        padding: 5px;
+    }
+</style>
