2653493d8253270d421f8f1b4b2f63897271465f
SweetLollipop <137623637@qq.com>
2022-01-27 22:53:28 +0800

    12_src_全局事件总线

---
+++ 12_src_全局事件总线/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app">
+        <h1>{{msg}}</h1>
+        <School/>
+        <Student/>
+    </div>
+</template>
+
+<script>
+    // 引入组件
+    import School from './components/School.vue'
+    import Student from './components/Student.vue'
+    export default {
+        name:'App',
+        data(){
+            return{
+               msg:'你好啊!', 
+            }
+        },
+        components:{
+            School,
+            Student,
+        },
+    }
+</script>
+
+<style scoped>
+    .app{
+        background-color: gray;
+        padding: 5px;
+    }
+</style>
