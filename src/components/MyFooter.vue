0652ad93cd4e6e6e3054887a0f301dfe6b9b4205
SweetLollipop <137623637@qq.com>
2022-01-27 23:31:55 +0800

    13_TodoList事件总线

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="todo-footer" v-show="total">
+        <label>
+        <!-- <input type="checkbox" :checked="isAll" @change="checkAll"/> -->
+        <input type="checkbox" v-model="isAll"/>
+        </label>
+        <span>
+        <span>已完成{{doneTotal}}</span> / 全部{{total}}
+        </span>
+        <button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'MyFooter',
+        // props:['todos','checkAllTodo','clearAllTodo'],
+        props:['todos'],
+        computed:{
+            total(){
+                return this.todos.length
+            },
+            // doneTotal(){
+            //     let i = 0
+            //     this.todos.forEach((todo)=>{
+            //         if(todo.done) i++
+            //     })
+            //     return i
+            // }
+            doneTotal(){
+                /* const x = this.todos.reduce((pre,current)=>{
+                    console.log("@",pre,current)
+                    return pre + (current.done ? 1 : 0)
+                },0) */
+                /* return this.todos.reduce((pre,todo)=>{
+                     return pre + (todo.done ? 1 : 0)
+                },0) */
+                return this.todos.reduce((pre,todo) => pre + (todo.done ? 1 : 0), 0) 
+            },
+            // 只读取，用简写
+            /* isAll(){
+                return this.doneTotal === this.total && this.total >0
+            } */
+            //读写
+            isAll:{
+                get(){
+                    return this.doneTotal === this.total && this.total >0
+                },
+                set(val){
+                    // this.checkAllTodo()
+                    this.$emit('checkAllTodo',val)
+                }
+            }
+        },
+        methods:{
+            /* checkAll(e){
+                this.checkAllTodo(e.target.checked)
+            } */
+            clearAll(){
+                // this.clearAllTodo()
+                this.$emit('clearAllTodo')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /*footer*/
+    .todo-footer {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+    }
+
+    .todo-footer label {
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+    }
+
+    .todo-footer label input {
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+    }
+
+    .todo-footer button {
+    float: right;
+    margin-top: 5px;
+    }
+</style>
