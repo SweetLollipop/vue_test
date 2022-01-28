50dc92f30c8c1f0c16334eaeedb699f0901b2c6e
SweetLollipop <137623637@qq.com>
2022-01-28 23:39:41 +0800

    092过渡效果

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
         <Test/>
+        <Test2/>
     </div>
 </template>
 
 <script>
     // 引入组件
     import Test from './components/Test.vue'
+    import Test2 from './components/Test2.vue'
     export default {
         name:'App',
-        components:{Test}
+        components:{Test,Test2}
     }
 </script>
 
---
+++ src/components/Test2.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <button @click="isShow=!isShow">显示/隐藏</button>
+        <transition name="hello" appear>
+            <h1 v-show="isShow">你好啊!</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Test',
+        data(){
+            return{
+                isShow:true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ h1{
+     background-color: orange;
+ }
+ 
+ /* 进入的起点|离开的终点 */
+ .hello-enter, .hello-leave-to{
+     transform: translate(-100%);
+ }
+ /* 进入的终点|离开的起点*/
+ .hello-enter-to, .hello-leave{
+     transform: translate(0px);
+ }
+ /* 进入的过渡|离开的过渡*/
+ .hello-enter-active, .hello-leave-active{
+    transition: 0.5s linear;
+ }
+</style>
