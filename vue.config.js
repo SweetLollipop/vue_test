3d6cbbd7cdb03f933c0da9fc5ce8271159721c4a
SweetLollipop <137623637@qq.com>
2022-01-30 23:03:49 +0800

    097配置代理_方式二

--- vue.config.js
+++ vue.config.js
@@ -21,8 +21,25 @@ module.exports = {
       subpage: 'src/main.js'
     },
     lintOnSave:true, // 关闭语法检查
-    //开启代理服务器
-    devServer: {
+    //开启代理服务器（方式一）
+    /* devServer: {
       proxy: 'http://localhost:5000'
+    } */
+    //开启代理服务器（方式二）
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          pathRewrite:{'^/api':''},  //路径重写：匹配/pai赋值为空字符串
+          ws: true,  //用于支持websocket
+          changeOrigin: true  //用于控制请求头中的host值
+        },
+        '/demo': {
+          target: 'http://localhost:5001',
+          pathRewrite:{'^/demo':''},  //路径重写：匹配/pai赋值为空字符串
+          ws: true,  //用于支持websocket
+          changeOrigin: true  //用于控制请求头中的host值
+        }
+      }
     }
   }
